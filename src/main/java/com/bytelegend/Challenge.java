66e79a7b61becf703dbbcfb88b4d09c56acab456
greek-zzf <49605925+greek-zzf@users.noreply.github.com>
2022-07-28 02:11:47 +0000

    Update at 2022-07-28T02:11:47.760994Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
 public class Challenge {
     public static void main() {
         System.out.println(createPutObjectRequest("bucket", "key", "utf-8", "plain/text", 1234L));
@@ -10,5 +12,13 @@ public class Challenge {
             String key,
             String contentEncoding,
             String contentType,
-            Long contentLength) {}
+            Long contentLength) {
+        return PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(contentType)
+                .contentLength(contentLength)
+                .contentEncoding(contentEncoding)
+                .build();
+    }
 }
