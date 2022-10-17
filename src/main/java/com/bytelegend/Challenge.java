25a043cfc2ea1e4f433c7983adb0a6e953cab1c0
pipiloveslife <Pain_Zzz@163.com>
2022-10-17 06:21:15 +0000

    Update at 2022-10-17T06:21:15.286790Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,5 +10,7 @@ public class Challenge {
             String key,
             String contentEncoding,
             String contentType,
-            Long contentLength) {}
+            Long contentLength) {
+                return software.amazon.awssdk.services.s3.model.PutObjectRequest.builder().bucket(bucket).key(key).contentEncoding(contentEncoding).contentType(contentType).contentLength(contentLength).build();
+            }
 }
