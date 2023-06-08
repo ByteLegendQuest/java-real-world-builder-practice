9b5702492dcd3b28e9200f922e0c86f80944e99c
andyliukun <744059787@qq.com>
2023-06-08 03:18:20 +0000

    Update at 2023-06-08T03:18:20.059784Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,14 +1,19 @@
 package com.bytelegend;
 
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
 public class Challenge {
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println(createPutObjectRequest("bucket", "key", "utf-8", "plain/text", 1234L));
     }
 
-    public static software.amazon.awssdk.services.s3.model.PutObjectRequest createPutObjectRequest(
+    public static PutObjectRequest createPutObjectRequest(
             String bucket,
             String key,
             String contentEncoding,
             String contentType,
-            Long contentLength) {}
+            Long contentLength) {
+
+        return PutObjectRequest.builder().bucket(bucket).key(key).contentEncoding(contentEncoding).contentType(contentType).contentLength(contentLength).build();
+    }
 }
